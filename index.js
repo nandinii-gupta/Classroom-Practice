66c127345c8916c757e56fa10a49cff136e56e5e
nandinii-gupta <nandinigupta2236@gmail.com>
2026-02-04 11:45:40 +0530

    Added new code

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ console.log("hello world");
 const fs=require('fs');
 fs.writeFileSync('example.txt',"this is an example file created using Node.js",
     function(e){
-        if(e) consloe.log("ye aaya error");
-        consloe.log("done");
+        if(e) console.log("ye aaya error");
+        console.log("done");
 });
-consloe.log("file created successfully");
-consloe.log("file system operation completeed");
+console.log("file created successfully");
+console.log("file system operation completeed");
---
+++ path.js
@@ -0,0 +1,5 @@
+const path = require('path');
+const filePath = 'C:\Users\nandi\OneDrive\Desktop\Classroom Practice\index.js';
+
+const dirName = path.dirname(filePath);
+console.log(dirName);
